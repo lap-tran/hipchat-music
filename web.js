2bb4e73a53a0bf426f3b1c29e6c86daddbfc6dfb
Sami Jaatinen <sami.jaatinen@atlassian.com>
2015-08-20 19:44:47 +0700

    only return id, title, duration and default thumbnail in room/id search

--- web.js
+++ web.js
@@ -86,31 +86,67 @@ addon.webhook('room_message', /^\/music\sadd\s.*$/, function *() {
 
   // Get videoId
   var msg = this.message.message;
-  var videoId = msg.substring(11); // default "/music add videoId"
-  var vIndex = videoId.indexOf("v=");
-  if (vIndex >= 0) {
-    var andIndex = videoId.indexOf("&", vIndex);
-    if (andIndex <= 0) {
-      videoId = videoId.substring(vIndex + 2);
-    } else {
-      videoId = videoId.substring(vIndex + 2, andIndex);
+  var searchParam = msg.substring(11); // default "/music add videoId"
+
+    var apiKey = 'AIzaSyA7Mc1ZQMzlQihPgjYE2v2ktxJ-ODLEl0c';
+    var query = searchParam;
+    if (query == null) {
+        this.body = 'no search parameter p defined.';
+        return;
     }
-  }
+    var queryUrl = url.parse(query, true);
+    var response = null;
+
+    if (queryUrl &&
+            queryUrl.host != null &&
+            queryUrl.host.indexOf('youtube.com') != -1 &&
+            queryUrl.query.v) {
+
+        response = yield request.get({
+            url: 'https://www.googleapis.com/youtube/v3/videos',
+            qs: {
+                key: apiKey,
+                part: 'snippet',
+                id: queryUrl.query.v
+            }
+        });
+    } else {
+        response = yield request.get({
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            qs: {
+                key: apiKey,
+                part: 'snippet',
+                type: 'video',
+                maxResults: 1,
+                q: query,
+                videoCategoryId: 'music' // not sure if this makes results better or worse
+            }
+        });
+    }
+
+    var responseJson = JSON.parse(response.body);
+
+    if (response.body == null || responseJson.items.length === 0) {
+        return;
+    }
+    var id = responseJson.items[0].id;
+    var videoId = id.videoId ? id.videoId : id;
+    var title = responseJson.items[0].snippet.title;
 
   // Add video to playlist of the room
   var roomId = this.room.id;
   redisClient.lrange('room-' + roomId, 0, -1, function(err, reply) {
     if (reply.indexOf(videoId) < 0) {
-      redisClient.rpush(['room-' + roomId, videoId], function (err, reply) {
-        that.roomClient.sendNotification('Added youtude id into the playlist: "' + videoId + '"');
+      redisClient.rpush([roomId, videoId], function (err, reply) {
+        that.roomClient.sendNotification('Added a song into the playlist: "' + title + '"');
 
         // Get current videos in the room
-        redisClient.lrange('room-' + roomId, 0, -1, function(err, reply) {
-          that.roomClient.sendNotification('The playlist are now: "' + reply + '"');
+        redisClient.lrange(roomId, 0, -1, function(err, reply) {
+          that.roomClient.sendNotification('The playlist is now: "' + reply + '"');
         });
       });
     } else {
-      that.roomClient.sendNotification('The song "' + videoId + '" already exsists in the playlist');
+      that.roomClient.sendNotification('The song "' + title + '" already exsists in the playlist');
     }
   });
   
@@ -135,6 +171,45 @@ app.use(route.get('/glance', function *(next){
 app.use(route.get('/page', function *(){
     yield send(this, __dirname + "/templates/index.html");
 }));
+
+app.use(route.get('/room/:id', function *(id){
+    var roomId = 'room-' + id;
+    // var apiKey = process.env.YOUTUBE_API_KEY;
+    var apiKey = 'AIzaSyA7Mc1ZQMzlQihPgjYE2v2ktxJ-ODLEl0c';
+
+    var videoIds = null;
+
+    var response = {body: ''};
+
+    var reply = yield coRedisClient.lrange(roomId, 0, -1);
+
+    if (reply != undefined) {
+        videoIds = reply.join(',');
+        if (videoIds == null || videoIds == undefined) {
+          videoIds = '';
+        }
+    }
+
+    if (videoIds !== '') {
+          response = yield request.get({
+              url: 'https://www.googleapis.com/youtube/v3/videos',
+              qs: {
+                  key: apiKey,
+                  part: 'contentDetails,snippet',
+                  type: 'video',
+                  id: videoIds,
+                  fields: 'items(id,snippet(title,thumbnails(default)),contentDetails(duration))',
+                  videoCategoryId: 'music' // not sure if this makes results better or worse
+              }
+          });
+        }
+
+    this.body = response.body;
+}));
+
+addon.webhook('room_message', /^\/roomid$/, function *() {
+  this.roomClient.sendNotification('your room id is: "' + this.room.id + '"');
+});
 
 app.use(route.get('/template', function *(){
   yield send(this, __dirname + "/templates/index.html");
