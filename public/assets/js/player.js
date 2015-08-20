5253c9dc18e2d35d012a1a3ec52261fc36bf7f90
Nguyen Hong Phong <phnguyen@atlassian.com>
2015-08-20 19:31:34 +0700

    Play video as background

---
+++ public/assets/js/player.js
@@ -0,0 +1,28 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '0',
+        width: '0',
+        videoId: 'nfWlot6h_JM',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
